b910b36ac9dac427ea717bfc9a1f00727e2e458a
tbc-beren <beren.themoonlightshade@gmail.com>
2021-03-04 08:47:26 +0100

    Updated license notice

--- include/BCuXml/BCuXmlNode.hpp
+++ include/BCuXml/BCuXmlNode.hpp
@@ -7,6 +7,10 @@
 * Mozilla Public License Version 2.0
 * https://github.com/tbc-beren/BCJson/blob/master/LICENSE
 *
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+* If a copy of the MPL was not distributed with this file, You can obtain one at
+* https://mozilla.org/MPL/2.0/.
+*
 */
 #pragma once
 
